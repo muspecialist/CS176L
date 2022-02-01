9cf1445fec4bd19b6f713e230a33a22607983df1
muspecialist <55097482+muspecialist@users.noreply.github.com>
2022-02-01 12:13:53 -0500

    HousePainting version 1

---
+++ HousePainting.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class HousePainting {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//Define Variables
+		double lengthOfHouse = 0;
+		//Inputs
+		System.out.println("Please enter length of house: ");
+		lengthOfHouse = sc.nextDouble();
+		System.out.println(lengthOfHouse);
+		//Calculations
+		
+		//Outputs
+		
+		
+
+	}
+
+}
