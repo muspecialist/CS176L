aaa25f9863b84227243c79230c9e1b2066f014f4
muspecialist <55097482+muspecialist@users.noreply.github.com>
2023-09-07 16:08:21 -0400

    my first java program version 1

---
+++ hello.java
@@ -0,0 +1,17 @@
+package helloWorld;
+
+public class hello 
+{
+
+	public static void main(String[] args) 
+	{
+		//CS176L
+
+		//Gil Eckert
+
+		//helloWorld
+		System.out.println("Hello World");
+
+	}
+
+}
